ae317fe65c6df1a2501d1b45838fa2c3c05e8beb
Darcy Keys <dkeys8722@gmail.com>
2022-05-19 11:50:11 +1000

    added displaying sprite

--- HelloSprite.py
+++ HelloSprite.py
@@ -3,7 +3,7 @@ import time
 pygame.init()
 
 screen = pygame.display.set_mode((640,480),0,32)
-pygame.display.set_caption("Hello Pygame Window")
+pygame.display.set_caption("Hello Pygame Sprite")
 screen.fill((0,0,0))
 
 sprite1 = pygame.image.load("Images/amogus.png")
@@ -16,4 +16,6 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # command to actually quit the game by clicking the X button
             game_over = True
+    screen.blit(sprite1,(0,0))
+    pygame.display.update()
 pygame.quit()
